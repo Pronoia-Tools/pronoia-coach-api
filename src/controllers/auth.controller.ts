164f9261fe01e5b12f39ab96e6832ad78733b444
Tomas Obregon <tomasobregon@Air-de-Tomas.lan>
2021-09-30 18:54:33 -0300

    bug-fix - error code string email-already-exists

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,92 +1,100 @@
-export {}
+export {};
 /** Node Modules */
-const httpStatus = require('http-status');
+const httpStatus = require("http-status");
 const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
 
 /** Custom Modules */
-const admin = require('../config/firebaseAdmin').firebase_admin_connect();
-const catchAsync = require('../utils/catchAsync');
-const ApiError = require('../utils/ApiError');
-const pick = require('../utils/pick');
+const admin = require("../config/firebaseAdmin").firebase_admin_connect();
+const catchAsync = require("../utils/catchAsync");
+const ApiError = require("../utils/ApiError");
+const pick = require("../utils/pick");
 
-import { isError } from 'joi';
+import { isError } from "joi";
 /** Schemas */
-import { User } from '../models/User';
+import { User } from "../models/User";
 
 const register = catchAsync(async (req: any, res: any) => {
   let body = req.body;
   let { firstname, lastname, email, password, country } = body;
 
   const userPass: any[] = [];
-  await admin.auth().createUser({
-    email: email,
-    password: password,
-    displayName: firstname + ' ' + lastname,
-    disabled: false,
-  }).then((userCredential: any) => {
-    // Signed in
-    userPass.push(userCredential.providerData[0].uid) 
-    userPass.push(userCredential.uid)
-  })
-  .catch((error: any) => {
-    let code = httpStatus.INTERNAL_SERVER_ERROR;
-    let message = error.message;
-    if (error.code === "auth/email-already-in-use") {
-      code = httpStatus.BAD_REQUEST;
-      message = error.message;
-    } else if (error.code === "auth/invalid-email"){
-      code = httpStatus.BAD_REQUEST;
-      message = error.message;
-    } else if (error.code === "auth/operation-not-allowed") {
-      code = httpStatus.FORBIDDEN;
-      message = error.message;
-    } else if (error.code === "auth/weak-password") {
-      code = httpStatus.BAD_REQUEST;
-      message = error.message;
-    } 
-    throw new ApiError(code, error.message);
-  });
-  
+  await admin
+    .auth()
+    .createUser({
+      email: email,
+      password: password,
+      displayName: firstname + " " + lastname,
+      disabled: false,
+    })
+    .then((userCredential: any) => {
+      // Signed in
+      userPass.push(userCredential.providerData[0].uid);
+      userPass.push(userCredential.uid);
+    })
+    .catch((error: any) => {
+      let code = httpStatus.INTERNAL_SERVER_ERROR;
+      let message = error.message;
+      if (error.code === "auth/email-already-exists") {
+        code = httpStatus.BAD_REQUEST;
+        message = error.message;
+      } else if (error.code === "auth/invalid-email") {
+        code = httpStatus.BAD_REQUEST;
+        message = error.message;
+      } else if (error.code === "auth/operation-not-allowed") {
+        code = httpStatus.FORBIDDEN;
+        message = error.message;
+      } else if (error.code === "auth/weak-password") {
+        code = httpStatus.BAD_REQUEST;
+        message = error.message;
+      }
+      throw new ApiError(code, error.message);
+    });
+
   const user = new User();
   user.firstName = firstname;
   user.lastName = lastname;
   user.email = email;
   user.country = country;
   user.uuid = userPass[1];
-  
-  await user.save()
-  .catch((error: any) => {
+
+  await user.save().catch((error: any) => {
     throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, error.message);
   });
 
   res.status(httpStatus.OK).json({
-    user: pick(user, ['id', 'firstName', 'lastName', 'email', 'country'])
+    user: pick(user, ["id", "firstName", "lastName", "email", "country"]),
   });
-  
 });
 
 const login = catchAsync(async (req: any, res: any) => {
   const { email, password } = req.body;
 
-  const currentUser = await User.findOne({email: email})
-  if(!currentUser) {
-    throw new ApiError(httpStatus.NOT_FOUND, "The user with email `${email}` is not registerd");
+  const currentUser = await User.findOne({ email: email });
+  if (!currentUser) {
+    throw new ApiError(
+      httpStatus.NOT_FOUND,
+      "The user with email `${email}` is not registerd"
+    );
   }
 
-  let token = '';
+  let token = "";
   await signInWithEmailAndPassword(getAuth(), email, password)
-  .then((userCredential: any) => {
-    token = userCredential.user.accessToken
-  })
-  .catch((error: any) => {
-    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, error.message);
-  });
+    .then((userCredential: any) => {
+      token = userCredential.user.accessToken;
+    })
+    .catch((error: any) => {
+      throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, error.message);
+    });
 
-  if(token === '') throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'Could not retrieve token.');
+  if (token === "")
+    throw new ApiError(
+      httpStatus.INTERNAL_SERVER_ERROR,
+      "Could not retrieve token."
+    );
 
   res.status(httpStatus.OK).json({
-    user: pick(currentUser, ['firstName', 'lastName', 'email', 'country']),
-    token: token
+    user: pick(currentUser, ["firstName", "lastName", "email", "country"]),
+    token: token,
   });
 });
 
