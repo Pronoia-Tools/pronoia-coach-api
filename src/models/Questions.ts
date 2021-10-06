cf161891756625e7841d903d71f41e48cd3aa60c
Tomas Obregon <tomasobregon@Air-de-Tomas.lan>
2021-10-05 23:51:12 -0300

    created new model wustions

---
+++ src/models/Questions.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, ManyToMany, Column, BaseEntity } from "typeorm";
+import { User } from './User'
+
+@Entity()
+export class Questions extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToMany(() => User, user => user.workbooks)
+    author: User;
+
+    @Column({length: 100})
+    question: string;
+
+    @Column({length: 100})
+    answer: string;
+
+}
