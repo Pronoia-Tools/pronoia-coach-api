d7fe749f05dee8b434849d995f21cd7366668ef1
Juan Pablo <jp.uruza.t@gmail.com>
2021-10-08 16:27:48 -0300

    API for session perssistance

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,15 +3,15 @@ const express = require('express');
 const validate = require('../middlewares/validate');
 const authValidation = require('../validations/auth.validation');
 const authController = require('../controllers/auth.controller');
-// const auth = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 // router.get('/', (req: any, res:any) => {return res.status(200);})
 router.post('/register', validate(authValidation.register), authController.register);
 router.post('/login', validate(authValidation.login), authController.login);
-
-// router.post('/logout', validate(authValidation.logout), authController.logout);
+router.post('/refresh', auth(), authController.refresh);
+router.post('/logout', auth(), authController.logout);
 // router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
 // router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
 // router.post('/reset-password', validate(authValidation.resetPassword), authController.resetPassword);
