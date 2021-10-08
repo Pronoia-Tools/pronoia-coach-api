f8b717b09463b4c4cd67543d6ca988647480fc4d
Juan Pablo <jp.uruza.t@gmail.com>
2021-10-08 13:11:42 -0300

    Autho on api endpoints

--- src/routes/workbook.route.ts
+++ src/routes/workbook.route.ts
@@ -4,9 +4,9 @@ const workbookController = require("../controllers/workbook.controller");
 const router = express.Router();
 const auth = require("../middlewares/auth");
 
-router.get("/:id", workbookController.get);
-router.get("/", workbookController.getAll);
-router.post("/", workbookController.post);
+router.get("/:id", auth(), workbookController.get);
+router.get("/", auth(), workbookController.getAll);
+router.post("/", auth(), workbookController.post);
 router.put("/:id", auth(), workbookController.put);
 router.delete("/:id", auth(), workbookController.remove);
 // router.post('/login', validate(authValidation.login), authController.login);
