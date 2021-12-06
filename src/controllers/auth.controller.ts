06c4149daa8ed1e4d9dd944ea7422a591e506c04
Federico Giovenco <giovencofede@gmail.com>
2021-12-06 11:41:26 -0300

    Business name finished backednd (#30)
    * Business name finished backednd
    
    * Fix migraiton
    
    Co-authored-by: Juan Pablo <jp.uruza.t@gmail.com>

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -28,7 +28,7 @@ import { User } from "../models/User";
 
 const register = catchAsync(async (req: any, res: any) => {
   let body = req.body;
-  let { firstname, lastname, email, password, country, notify, listing_badge, newsletter, pre_launch } = body;
+  let { firstname, lastname, email, password, country, notify, listing_badge, newsletter, pre_launch, businessname } = body;
  
   const userPass: any[] = [];
   await admin
@@ -80,6 +80,7 @@ const register = catchAsync(async (req: any, res: any) => {
   user.newsletter = newsletter;
   user.pre_launch = pre_launch;
   user.stripeCustomerId = stripe_customer.id;
+  user.businessname = businessname
 
   await user.save().catch((error: any) => {
     throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, error.message);
@@ -150,7 +151,7 @@ const register = catchAsync(async (req: any, res: any) => {
 });
 
 const updateUser = catchAsync(async (req: any, res: any) => {
-  const { firstname, lastname, currentEmail, newEmail, country, newPassword, currentPassword } = req.body;
+  const { firstname, lastname, currentEmail, newEmail, country, newPassword, currentPassword, businessname } = req.body;
 
   const currentUser = await User.findOne({email:currentEmail});
   if (!currentUser) {
@@ -177,7 +178,8 @@ const updateUser = catchAsync(async (req: any, res: any) => {
     firstName:firstname,
     lastName:lastname,
     country:country,
-    email:newEmail
+    email:newEmail,
+    businessname:businessname
   });
   const results = await User.save(currentUser);
 
@@ -278,7 +280,7 @@ const login = catchAsync(async (req: any, res: any) => {
     });
   }
   res.status(httpStatus.OK).json({
-    user: pick(currentUser, ["firstName", "lastName", "email", "country", "authorized"]),
+    user: pick(currentUser, ["firstName", "lastName", "email", "country", "authorized","businessname"]),
     token: token,
     customTokenAuthFirebase
   });
@@ -297,7 +299,7 @@ const refresh = catchAsync(async (req: any, res: any) => {
       console.log('Error creating custom token:', error);
     });
   res.status(httpStatus.OK).json({
-    user: pick(req.currentUser, ["firstName", "lastName", "email", "country"]),
+    user: pick(req.currentUser, ["firstName", "lastName", "email", "country","businessname"]),
     token: req.get('authorization').replace('Bearer ', ''),
     customTokenAuthFirebase
   });
